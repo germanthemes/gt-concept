3297ca66c954e57fa9c06168dac70e6aa2c0a688
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-01-06 21:10:27 +0100

    added WPTT Webfont loader to embed default theme fonts

--- functions.php
+++ functions.php
@@ -137,10 +137,34 @@ add_action( 'wp_enqueue_scripts', 'gt_concept_scripts' );
 * Enqueue theme fonts.
 */
 function gt_concept_theme_fonts() {
-	wp_enqueue_style( 'gt-concept-theme-fonts', get_template_directory_uri() . '/assets/css/theme-fonts.css', array(), '20200826' );
+	$fonts_url = gt_concept_get_fonts_url();
+
+	// Load Fonts if necessary.
+	if ( $fonts_url ) {
+		require_once get_theme_file_path( 'inc/wptt-webfont-loader.php' );
+		wp_enqueue_style( 'gt-concept-theme-fonts', wptt_get_webfont_url( $fonts_url ), array(), '20210105' );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'gt_concept_theme_fonts', 1 );
 add_action( 'enqueue_block_editor_assets', 'gt_concept_theme_fonts', 1 );
+
+
+/**
+ * Retrieve webfont URL to load fonts locally.
+ */
+function gt_concept_get_fonts_url() {
+	$font_families = array(
+		'Roboto:400,400italic,700,700italic',
+	);
+
+	$query_args = array(
+		'family'  => urlencode( implode( '|', $font_families ) ),
+		'subset'  => urlencode( 'latin,latin-ext' ),
+		'display' => urlencode( 'swap' ),
+	);
+
+	return apply_filters( 'gt_concept_get_fonts_url', add_query_arg( $query_args, 'https://fonts.googleapis.com/css' ) );
+}
 
 
 /**
